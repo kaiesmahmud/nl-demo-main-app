11dde6ce9b9c2fd37f1b12674284210188fdfeea
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-27 18:35:21 +0600

    component created

---
+++ src/lib/components/home-components/HeaderSliderCmpnt.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+    // Default theme
+    import '@splidejs/svelte-splide/css';
+
+    // or other themes
+    import '@splidejs/svelte-splide/css/skyblue';
+    import '@splidejs/svelte-splide/css/sea-green';
+
+    // or only core styles
+    import '@splidejs/svelte-splide/css/core';
+    let slides = [
+        "https://d20q60vkvwzi8p.cloudfront.net/gallery/Custom-Diamond-New-York-Skyline-Key-Charm-Pendant-64105-Stone-6.jpg",
+        "https://d20q60vkvwzi8p.cloudfront.net/gallery/Custom-Diamond-New-York-Skyline-Key-Charm-Pendant-64105-Stone-6.jpg",
+        "https://d20q60vkvwzi8p.cloudfront.net/gallery/Custom-Diamond-New-York-Skyline-Key-Charm-Pendant-64105-Stone-6.jpg"
+    ]
+
+    let options={
+    type : 'loop',
+    autoplay: true,
+    perPage : 2,
+    interval: 1000,
+    arrows: false
+  }
+</script>
+<Splide  options={options}
+  aria-label="My Favorite Images" class=" cursor-pointer">
+    {#each slides as item}
+        <SplideSlide>
+        <img src={item} alt="Imsage 1"/>
+        </SplideSlide>
+         <!-- content here -->
+    {/each}
+    <SplideSlide>
+      <img src="https://d20q60vkvwzi8p.cloudfront.net/gallery/Custom-Diamond-New-York-Skyline-Key-Charm-Pendant-64105-Stone-2.jpg" alt="s 2"/>
+    </SplideSlide>
+  </Splide>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+    // Default theme
+    import '@splidejs/svelte-splide/css';
+
+    // or other themes
+    import '@splidejs/svelte-splide/css/skyblue';
+    import '@splidejs/svelte-splide/css/sea-green';
+
+    // or only core styles
+    import '@splidejs/svelte-splide/css/core';
+    import HeaderSliderCmpnt from '../lib/components/home-components/HeaderSliderCmpnt.svelte';
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<HeaderSliderCmpnt/>
