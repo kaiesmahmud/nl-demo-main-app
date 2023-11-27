46e0279113b0e1f48497d9079f64898ce40a4cec
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-27 20:55:46 +0600

    ..

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,5 @@
     import '@splidejs/svelte-splide/css/core';
     import HeaderSliderCmpnt from '../lib/components/home-components/HeaderSliderCmpnt.svelte';
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <HeaderSliderCmpnt/>
