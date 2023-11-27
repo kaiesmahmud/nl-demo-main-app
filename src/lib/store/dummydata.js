58662b9b8f7eddaab150170d480600413c7b44ea
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-27 23:08:04 +0600

    celebrity slider added

--- src/lib/store/dummydata.js
+++ src/lib/store/dummydata.js
@@ -6,7 +6,52 @@ export let HomePageData = {
         {
             id:'02', url: "/slider-img-1.jpg"
         },
+    ],
+    celebritySliders : [
+        {
+            name:"erica mina",
+            img: "https://d186vdbjetg11u.cloudfront.net/static/media/11._erica_mena_bracelets_-290.aec15e7180c37b98e395.webp",
+            productimg : "https://d186vdbjetg11u.cloudfront.net/static/media/11._Erica-Mena-Tennis-Bracelet-Set-67245-Diamond-4.7e4c06d33880992808b1.webp",
+            productLink: ""
+        },
+        {
+            name:"mark wahlberg ",
+            img: "https://d186vdbjetg11u.cloudfront.net/static/media/10-MARK-WAHLBERG-memorial-290.2907ee4ad7b9cc6d08e6.webp",
+            productimg : "https://d186vdbjetg11u.cloudfront.net/static/media/10-MARK-WAHLBERG-memorial-pendant.8e0234c496c9a787318a.webp",
+            productLink: ""
+        },
+        {
+            name:"Erica mina",
+            img: "https://d186vdbjetg11u.cloudfront.net/static/media/12.-erica-mena-ring-290.b156aec5b74f85fc5c91.webp",
+            productimg : "https://d186vdbjetg11u.cloudfront.net/static/media/12._Miami-Wrap-Around-Diamond-Ring-67986-Anniversary-Fashion-1.7f7beb479754ea8b6bc4.webp",
+            productLink: ""
+        },
+        {
+            name:"Joynar Lucas",
+            img: "https://d186vdbjetg11u.cloudfront.net/static/media/joyner_lucas-290.222d794880eaba5875ca.webp",
+            productimg : "https://d186vdbjetg11u.cloudfront.net/static/media/joyner_lucas_pendant_66359.8cf6ed66fe4dab97595f.webp",
+            productLink: ""
+        },
+        {
+            name:"mr. beast",
+            img: "https://d186vdbjetg11u.cloudfront.net/static/media/mr.beast-290.5068b16b04d553178bba.webp",
+            productimg : "https://d20q60vkvwzi8p.cloudfront.net/gallery/Platinum-YouTube-Pendant-68526-Metal-1.jpg",
+            productLink: ""
+        },
+        {
+            name:"Kodak black",
+            img: "https://d186vdbjetg11u.cloudfront.net/static/media/KODAK_BLACK__A_-290.5d78bf7b4be22571a1dd.webp",
+            productimg : "https://d186vdbjetg11u.cloudfront.net/static/media/Kodak_black_ring_(A)_68343.a33173e83254592c2075.webp",
+            productLink: ""
+        },
+        {
+            name:"Youn Ma",
+            img: "https://d186vdbjetg11u.cloudfront.net/static/media/Young_MA-290.5e2b0d93d9dc02414d6c.webp",
+            productimg : "https://d186vdbjetg11u.cloudfront.net/static/media/Young_MA_pendant.54c5a4fd3d8d2d159c56.webp",
+            productLink: ""
+        },
     ]
+
 }
 
 export let menCollectionData = {
