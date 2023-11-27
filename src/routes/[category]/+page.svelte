303bab4423105220cc61b9b12b2e38f552ceb47d
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-27 20:56:03 +0600

    new page created

---
+++ src/routes/[category]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { categoryDetails } from './../../lib/store/store.js';
+
+</script>
+    <div class="min-h-screen">
+        <p>this is a Category Page {$categoryDetails}</p>
+        
+    </div>
