aab6f8efaa0ad3251b22bfa64e4fb4deb382164f
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-27 22:29:17 +0600

    updated & final

--- src/lib/components/home-components/HeaderSliderCmpnt.svelte
+++ src/lib/components/home-components/HeaderSliderCmpnt.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { HomePageData } from './../../store/dummydata.js';
     import { Splide, SplideSlide } from '@splidejs/svelte-splide';
     // Default theme
     import '@splidejs/svelte-splide/css';
@@ -9,29 +10,31 @@
 
     // or only core styles
     import '@splidejs/svelte-splide/css/core';
-    let slides = [
-        "https://d20q60vkvwzi8p.cloudfront.net/gallery/Custom-Diamond-New-York-Skyline-Key-Charm-Pendant-64105-Stone-6.jpg",
-        "https://d20q60vkvwzi8p.cloudfront.net/gallery/Custom-Diamond-New-York-Skyline-Key-Charm-Pendant-64105-Stone-6.jpg",
-        "https://d20q60vkvwzi8p.cloudfront.net/gallery/Custom-Diamond-New-York-Skyline-Key-Charm-Pendant-64105-Stone-6.jpg"
-    ]
 
     let options={
     type : 'loop',
     autoplay: true,
-    perPage : 2,
+    // perPage : 2,
     interval: 3000,
-    // arrows: false
+    arrows: false,
+    // width: '100%',
   }
 </script>
 <Splide  options={options}
-  aria-label="My Favorite Images" class=" cursor-pointer">
-    {#each slides as item}
-        <SplideSlide>
-        <img src={item} alt="Imsage 1"/>
+  aria-label="My Favorite Images" class="w-full cursor-pointer">
+    {#each  HomePageData.headerSliders as item(item.id)}
+        <SplideSlide >
+          <div class=" rounded shadow-xl">
+            <img src={item.url} alt={item.id}>
+          </div>
         </SplideSlide>
          <!-- content here -->
     {/each}
-    <SplideSlide>
+    <!-- <SplideSlide>
       <img src="https://d20q60vkvwzi8p.cloudfront.net/gallery/Custom-Diamond-New-York-Skyline-Key-Charm-Pendant-64105-Stone-2.jpg" alt="s 2"/>
-    </SplideSlide>
+    </SplideSlide> -->
   </Splide>
+
+  <style>
+
+  </style>
