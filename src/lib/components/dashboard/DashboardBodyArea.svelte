598749c9f46a34ee411e5e0590a931ffa5fc422a
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-28 11:23:14 +0600

    store condition added

--- src/lib/components/dashboard/DashboardBodyArea.svelte
+++ src/lib/components/dashboard/DashboardBodyArea.svelte
@@ -1,6 +1,29 @@
 <script>
+    import { showDashboardBodyTopic } from "../../store/store";
 
+    let showArea = [
+        "my-order",
+        "my-account",
+        "edit-address",
+        "my-wishlists",
+        "compare-items",
+    ]
+    
 </script>
 <div class="w-full md:w-2/3 bg-black/10 rounded p-2 md:p-5">
-    Dashboard body
+    {#if $showDashboardBodyTopic === "my-order"}
+         <h1 class="text-xl font-bold">My Order</h1>
+    {/if}
+    {#if $showDashboardBodyTopic === "my-account"}
+         <h1 class="text-xl font-bold capitalize">my-account</h1>
+    {/if}
+    {#if $showDashboardBodyTopic === "edit-address"}
+         <h1 class="text-xl font-bold capitalize">edit-address</h1>
+    {/if}
+    {#if $showDashboardBodyTopic === "my-wishlists"}
+         <h1 class="text-xl font-bold capitalize">my-wishlists</h1>
+    {/if}
+    {#if $showDashboardBodyTopic === "compare-items"}
+         <h1 class="text-xl font-bold capitalize">compare-items</h1>
+    {/if}
 </div>
--- src/lib/components/dashboard/DashboardNavArea.svelte
+++ src/lib/components/dashboard/DashboardNavArea.svelte
@@ -1,30 +1,42 @@
 <script>
+	import { showDashboardBodyTopic } from './../../store/store.js';
     import Icon from '@iconify/svelte';
 
     let personalInfo = [
         {
             name:"My Account",
             icon: "octicon:person-24",
-            url:""
+            show:"my-account"
         },
         {
             name:"Edit Address",
             icon: "ion:location-outline",
-            url:""
+            show:"edit-address"
         },
     ]
     let myItems = [
         {
             name:"My Wishlists",
             icon: "tabler:heart",
-            url:""
+            show:"my-wishlists"
         },
         {
             name:"Compare Items",
             icon: "material-symbols-light:compare-arrows",
-            url:""
+            show:"compare-items"
         },
     ]
+    let showArea = [
+        "my-order",
+        "my-account",
+        "edit-address",
+        "my-wishlists",
+        "compare-items",
+    ];
+    const handleChangeDasBody = (arg) => {
+        showDashboardBodyTopic.set(arg)
+        console.log(arg)
+    }
 </script>
     <div class="w-full md:w-1/3 bg-black/10 rounded p-2 md:p-5">
         <h1 class="font-bold text-xl md:text-2xl">Hello ! User Name</h1>
@@ -32,7 +44,7 @@
         <h2 class="text-lg font-semibold my-4">Order Details</h2>
         <p>Order Information</p>
         <div class="flex flex-col gap-3 p-3">
-            <div class="p-3 bg-white/50 hover:bg-white flex items-center justify-between rounded">
+            <button on:click={()=>handleChangeDasBody("my-order")} class="p-3 bg-white/50 hover:bg-white flex items-center justify-between rounded">
                 <div class="flex items-center gap-3">
                     <div class="text-xl md:text-2xl">
                         <Icon icon="lets-icons:order-light" />                    
@@ -42,14 +54,14 @@
                 <div>
                     <Icon icon="ep:right" />
                 </div>
-            </div>
+            </button>
         </div>
         <h2 class="text-lg font-semibold my-3">My Account</h2>
 
         <p>Personal Information</p>
         <div class="flex flex-col gap-3 p-3" >
             {#each personalInfo as i}
-                <div class="p-3 bg-white/50 hover:bg-white flex items-center justify-between rounded">
+                <button on:click={()=>handleChangeDasBody(i.show)} class="p-3 bg-white/50 hover:bg-white flex items-center justify-between rounded">
                     <div class="flex items-center gap-3">
                         <div class="text-xl md:text-2xl">
                             <Icon icon={i.icon} />
@@ -59,13 +71,13 @@
                     <div>
                         <Icon icon="ep:right" />
                     </div>
-                </div>
+                </button>
             {/each}
         </div>
         <p>Other Information</p>
         <div class="flex flex-col gap-3 p-3" >
             {#each myItems as i}
-                <div class="p-3 bg-white/50 hover:bg-white flex items-center justify-between rounded">
+                <button on:click={()=>handleChangeDasBody(i.show)} class="p-3 bg-white/50 hover:bg-white flex items-center justify-between rounded">
                     <div class="flex items-center gap-3">
                         <div class="text-xl md:text-2xl">
                             <Icon icon={i.icon} />
@@ -75,7 +87,7 @@
                     <div>
                         <Icon icon="ep:right" />
                     </div>
-                </div>
+                </button>
             {/each}
         </div>
     </div>
