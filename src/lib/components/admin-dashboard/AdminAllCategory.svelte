2907ac405e45fbb558022ceae6dbd263ae1e0020
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-28 20:12:55 +0600

    admin panel updated

--- src/lib/components/admin-dashboard/AdminAllCategory.svelte
+++ src/lib/components/admin-dashboard/AdminAllCategory.svelte
@@ -1,7 +1,33 @@
 <script>
-
+    let navItems = [
+        {
+            name:"Men's Collection", url:"/men-collection"
+        },
+        {
+            name:"Women's Collection",url:"/women-collection"
+        },
+        {
+            name:"Custom Jewelry",url:"/custom-jewelry"
+        },
+        {
+            name:"Special Offer",url:"/special-offer"
+        },
+    ]
 </script>
 
 <div>
     <h1 class="font-bold text-xl md:text-2xl ">All Category</h1>
+    <div class="p-5 flex flex-col gap-5">
+        {#each navItems as i}
+        <div class="flex items-center justify-between w-full bg-white p-3  rounded">
+            <div>
+                {i.name}
+            </div>
+            <a href={i.url} class="hover:font-bold">
+                {i.url}
+            </a>
+        </div>
+
+        {/each}
+    </div>
 </div> 
--- src/lib/components/admin-dashboard/AdminAnalytics.svelte
+++ src/lib/components/admin-dashboard/AdminAnalytics.svelte
@@ -1,7 +1,41 @@
 <script>
-
+    let visitors = [
+        {
+            country: "Netherland",
+            visitors:231
+        },
+        {
+            country: "USA",
+            visitors:234
+        },
+        {
+            country: "IceLand",
+            visitors:152
+        },
+        {
+            country: "Australia",
+            visitors:765
+        },
+    ]
+    
 </script>
 
 <div>
     <h1 class="font-bold text-xl md:text-2xl ">Analytics</h1>
+    <h1 class=" text-xl md:text-2xl ">All Analytics will Be shown Here</h1>
+    <div class="flex flex-col md:flex-row gap-5">
+        <div class="flex flex-col gap-4 p-3 md:p-5 text-start items-start w-1/2">
+            {#each visitors as i}
+            <div class=" font-bold text-xl flex justify-between items-center w-full border-b-2 my-2 border-slate-800">
+                <p>
+                    {i.country}
+                </p>
+                <p>
+                    {i.visitors}
+                </p>
+            </div>
+            {/each}
+        </div>
+        
+    </div>
 </div>
