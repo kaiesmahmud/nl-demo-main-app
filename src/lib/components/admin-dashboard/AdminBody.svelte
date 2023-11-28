9370eac25f16e508b390ba6c1e75a604658caa49
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-28 18:59:02 +0600

    sub components added

--- src/lib/components/admin-dashboard/AdminBody.svelte
+++ src/lib/components/admin-dashboard/AdminBody.svelte
@@ -1,16 +1,35 @@
 <script>
+	import AdminAnalytics from './AdminAnalytics.svelte';
     import { showAdminNavItem } from "../../store/store";
+    import AdminAllCategory from './AdminAllCategory.svelte';
+    import AdminAllProducts from './AdminAllProducts.svelte';
+    import AdminAllUsers from './AdminAllUsers.svelte';
+    import AdminAllOrders from './AdminAllOrders.svelte';
 
     let showItemsName = [
-        "Analytics",
-        "All-Category",
-        "All-Products",
-        "All-Users",
-        "All-Orders",
+        "analytics",
+        "all-category",
+        "all-products",
+        "all-users",
+        "all-orders",
     ]
     
 </script>
 
-<section class="w-full md:w-2/3 bg-black/20 rounded shadow-lg">
-    Admin Body {$showAdminNavItem}
+<section class="w-full md:w-2/3 bg-black/20 rounded shadow-lg p-3 md:p-5">
+    {#if $showAdminNavItem === "analytics"}
+        <AdminAnalytics/>
+    {/if}
+    {#if $showAdminNavItem === "all-category"}
+        <AdminAllCategory/>
+    {/if}
+    {#if $showAdminNavItem === "all-products"}
+        <AdminAllProducts/>
+    {/if}
+    {#if $showAdminNavItem === "all-users"}
+        <AdminAllUsers/>
+    {/if}
+    {#if $showAdminNavItem === "all-orders"}
+        <AdminAllOrders/>
+    {/if}
 </section>
