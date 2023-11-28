159652051da0839f39bedf81d20bdb7c9c8ac889
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-28 18:48:19 +0600

    working on admin dashboard

---
+++ src/lib/components/admin-dashboard/AdminBody.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { showAdminNavItem } from "../../store/store";
+
+    let showItemsName = [
+        "Analytics",
+        "All-Category",
+        "All-Products",
+        "All-Users",
+        "All-Orders",
+    ]
+    
+</script>
+
+<section class="w-full md:w-2/3 bg-black/20 rounded shadow-lg">
+    Admin Body {$showAdminNavItem}
+</section>
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,38 @@
- <p>Admin Page</p>
+<script>
+    import AdminBody from "../../lib/components/admin-dashboard/AdminBody.svelte";
+    import AdminNav from "../../lib/components/admin-dashboard/AdminNav.svelte";
+    let adminNavItems = [
+        {
+            name:"analytics",
+            icon: "arcticons:adobe-analytics" ,
+            value:"analytics"
+        },
+        {
+            name:"All-Category",
+            icon: "material-symbols-light:category-outline" ,
+            value:"all-category"
+        },
+        {
+            name:"All-Products",
+            icon: "circum:shop",
+            value:"all-products"
+        },
+        {
+            name:"All-Users",
+            icon: "ph:users-thin",
+            value:"all-users"
+        },
+        {
+            name:"All-Orders",
+            icon: "fluent-mdl2:reservation-orders",
+            value:"all-orders"
+        },
+    ]
+    
+</script>
+
+<div class="min-h-screen flex flex-col md:flex-row gap-2 p-3 md:p-5 ">
+    <AdminNav {adminNavItems}  />
+    <AdminBody/>
+</div>
+
