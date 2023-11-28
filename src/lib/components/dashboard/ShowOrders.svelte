97582dac457cfdbfbce6c60a73753575990dce9f
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-28 12:46:14 +0600

    single items added

--- src/lib/components/dashboard/ShowOrders.svelte
+++ src/lib/components/dashboard/ShowOrders.svelte
@@ -1,26 +1,43 @@
 <script>
     import Icon from '@iconify/svelte';
+    import { showAllOrderLists, showPendingOrders } from '../../store/store';
+    import SingleOrderItem from './SingleOrderItem.svelte';
     export let i; 
 
     let open = false ;
     const handleOpen = () => {
         open = !open
     }
+    
 </script>
-
-         <!-- content here -->
-         <button on:click={handleOpen} class="w-full p-3 bg-white/50 hover:bg-white flex items-center justify-between rounded">
-             <div class="flex items-center gap-3">
-                 <div class="text-xl md:text-2xl">
-                     <Icon icon="fluent-mdl2:activate-orders" />
-                 </div>
-                 <p class="font-light">{i.name}</p>
-             </div>
-             <div>
-                 {#if open}
-                     <Icon icon="icon-park-outline:up" />
-                 {:else}
-                     <Icon icon="icon-park-outline:down" />
-                 {/if}
-             </div>
-         </button>
+    <div class="flex flex-col gap-3 w-full">
+        <button on:click={handleOpen} class="w-full p-3 bg-white/50 hover:bg-white flex items-center justify-between rounded">
+            <div class="flex items-center gap-3">
+                <div class="text-xl md:text-2xl">
+                    <Icon icon="fluent-mdl2:activate-orders" />
+                </div>
+                <p class="font-light">{i.name}</p>
+            </div>
+            <div>
+                {#if open}
+                    <Icon icon="icon-park-outline:up" />
+                {:else}
+                    <Icon icon="icon-park-outline:down" />
+                {/if}
+            </div>
+        </button>
+        {#if open && (i.value === "view-all-orders")}
+        <div class="flex flex-col gap-3 p-1">
+            {#each $showAllOrderLists as item}
+            <SingleOrderItem {item} />
+            {/each}
+        </div>
+        {/if}
+        {#if open && (i.value === "view-pending-orders")}
+        <div class="flex flex-col gap-3 p-1">
+            {#each $showPendingOrders as item}
+                <SingleOrderItem {item} />
+            {/each}
+        </div>
+        {/if}
+    </div>
