b6e550ec976e6685a148cf750a89db2e6b0844d2
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-30 20:43:41 +0600

    updating new ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,11 @@
     })
 
 </script>
-<div>
-    <Navbar/>
-    <slot/>
-    <Footer/>
+<div class="flex items-center justify-center" >
+
+    <div class="w-full lg:w-[80%]">
+        <Navbar/>
+        <slot/>
+        <Footer/>
+    </div>
 </div>
