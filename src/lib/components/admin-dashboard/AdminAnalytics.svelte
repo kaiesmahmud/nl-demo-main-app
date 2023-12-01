e6a44522355be66d6ead3127c42d7845e7eaad51
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-01 20:31:46 +0600

    working on analytics

--- src/lib/components/admin-dashboard/AdminAnalytics.svelte
+++ src/lib/components/admin-dashboard/AdminAnalytics.svelte
@@ -21,9 +21,19 @@
 </script>
 
 <div>
-    <h1 class="font-bold text-xl md:text-2xl ">Analytics</h1>
-    <h1 class=" text-xl md:text-2xl ">All Analytics will Be shown Here</h1>
-    <div class="flex flex-col md:flex-row gap-5">
+    <h1 class="font-bold text-xl md:text-2xl pb-3">Analytics</h1>
+    <hr>
+    <!-- <h1 class=" text-xl md:text-2xl ">All Analytics will Be shown Here</h1> -->
+    <div>
+        <div class="bg-black/5">
+            <p>Total Visitor</p>
+            <div class=" flex items-center gap-5">
+                <p>Monthly</p>
+                <p>2548</p>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="flex flex-col md:flex-row gap-5">
         <div class="flex flex-col gap-4 p-3 md:p-5 text-start items-start w-1/2">
             {#each visitors as i}
             <div class=" font-bold text-xl flex justify-between items-center w-full border-b-2 my-2 border-slate-800">
@@ -37,5 +47,5 @@
             {/each}
         </div>
         
-    </div>
+    </div> -->
 </div>
