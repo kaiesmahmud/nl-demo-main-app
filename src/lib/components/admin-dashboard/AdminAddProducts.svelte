dd3de8f845c3cdb684ff7fb2736709b0a20abcc2
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-03 00:57:06 +0600

    admin updated

--- src/lib/components/admin-dashboard/AdminAddProducts.svelte
+++ src/lib/components/admin-dashboard/AdminAddProducts.svelte
@@ -1,5 +1,30 @@
 <script>
-
+    let pName ,pUrl,pPrice,pRegPrice,pStock,pImg,pType,pCat,pCatId
+    const handleCancel=()=>{
+        pName=''
+        pUrl=''
+        pPrice=''
+        pRegPrice=''
+        pStock=''
+        pImg=''
+        pType=''
+        pCat=''
+        pCatId=''
+    }
+    const handleSubmit=()=>{
+        let data={
+            p_name:pName,
+            p_url:pUrl,
+            p_price:pPrice,
+            p_reg_price:pRegPrice,
+            p_stock:pStock,
+            p_img:pImg,
+            p_type:pType,
+            cat_id:pCatId,
+            cat_name:pCat
+        }
+        console.log(data)
+    }
 </script>
 <h1 class="font-bold text-xl md:text-2xl pb-3">Add Products</h1>
 <hr>
@@ -12,14 +37,14 @@
                 <div class="sm:col-span-3">
                     <label for="p_name" class="block text-sm font-medium leading-6 text-gray-900">Product name</label>
                     <div class="mt-2">
-                    <input type="text" name="p_name" id="p_name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <input bind:value={pName} type="text" name="p_name" id="p_name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
                 </div>
 <!-- ====================Category==================== -->
                 <div class="sm:col-span-3">
                     <label for="cat_id" class="block text-sm font-medium leading-6 text-gray-900">Category</label>
                     <div class="mt-2">
-                    <select id="cat_id" name="cat_id" autocomplete="cat_id" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                    <select bind:value={pCatId} id="cat_id" name="cat_id" autocomplete="cat_id" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
                         <option>/men-collection</option>
                         <option>/women-collection</option>
                         <option>/custom</option>
@@ -33,7 +58,7 @@
                     <div class="mt-2">
                         <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                         <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">traxnyc.com/category/</span>
-                        <input type="text" name="p_url" id="p_url"  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="diamond-ring">
+                        <input bind:value={pUrl} type="text" name="p_url" id="p_url"  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="diamond-ring">
                         </div>
                     </div>
                 </div>
@@ -65,7 +90,7 @@
                 <div class="col-span-full">
                 <label for="p_img" class="block text-sm font-medium leading-6 text-gray-900">Image URL</label>
                 <div class="mt-2">
-                    <input type="text" name="p_img" id="p_img"  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <input bind:value={pImg} type="text" name="p_img" id="p_img"  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
                 </div>
                 <div class="col-span-full text-center">
@@ -76,21 +101,21 @@
                 <div class="sm:col-span-2 sm:col-start-1">
                     <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
                     <div class="mt-2">
-                    <input type="number" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
+                    <input bind:value={pPrice} type="number" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
                     </div>
                 </div>
 
             <div class="sm:col-span-2">
                 <label for="region" class="block text-sm font-medium leading-6 text-gray-900">Regular Price</label>
                 <div class="mt-2">
-                <input type="number" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
+                <input bind:value={pRegPrice} type="number" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
 
             <div class="sm:col-span-2">
                 <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">Stock</label>
                 <div class="mt-2">
-                <input type="number" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
+                <input bind:value={pStock} type="number" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
             </div>
@@ -100,13 +125,8 @@
     </div>
   
     <div class="mt-6 flex items-center justify-end gap-x-6">
-      <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-      <button type="submit" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Upload</button>
+      <button on:click={handleCancel} type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+      <button on:click={handleSubmit} type="submit" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Upload</button>
     </div>
-  </form>
+</form>
 
-  <style>
-    /* input{
-        @apply ' font-sans';
-    } */
-  </style>
