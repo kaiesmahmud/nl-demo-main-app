a1564948a7719b184268d04c9e2498ab9ceac5b1
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-11 23:27:56 +0600

    css improved

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -19,7 +19,7 @@
             {#if $myCartData.length == 0}
                     <h1>No Products Selected</h1>
             {/if}
-            <div class="flex flex-col gap-3 w-full lg:w-2/3">
+            <div class="flex flex-col gap-3 w-full ">
                 {#each $myCartData as {p_id,p_name,p_quantity,cat_id,cat_name,p_price,p_img,p_type,p_reg_price,p_stock}}
                     <div class="p-2 bg-white shadow rounded">
                         <div class="flex flex-col md:flex-row items-center justify-between gap-2">
@@ -28,20 +28,19 @@
                                     <img src={p_img} alt="">
                                 </div>
                                 <div class="text-xs md:text-sm font-nunito ">
+                                    <p><span class="font-bold" >Name: </span>{p_name}</p>
+                                    <p><span class="font-bold">ID: </span>{p_id}</p>
                                     <div class="flex items-center justify-between py-2">
                                         <div>
-                                            <p><span class="font-bold">ID: </span>{p_id}</p>
-                                            <!-- <p><span class="font-bold">Stock: </span>{p_stock}</p> -->
-                                            <!-- <p class=" line-through opacity-50"><span class="font-bold" >Regular Price: </span>{p_reg_price}</p> -->
                                             <p><span class="font-bold " >Price: </span>${p_price}</p>
                                             <p><span class="font-bold " >Quantity: </span>{p_quantity}</p>
+                                            <hr>
                                             <p><span class="font-bold " >Subtotal: </span>${productTotalPrice(p_price,p_quantity)}</p>
 
                                         </div>
                                         <div>
                                         </div>
                                     </div>
-                                    <p><span class="font-bold" >Name: </span>{p_name}</p>
                                 </div>
                             </div>
                         </div>
